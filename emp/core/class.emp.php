80be5f717923b2437597fc81279ea068fbe3f041
zeddie <zeddicus@myemp.us>
2014-01-07 17:28:33 +0200

    Update class.emp.php
    Cosmetic Changes.

--- emp/core/class.emp.php
+++ emp/core/class.emp.php
@@ -1,48 +1,42 @@
-<?php  if ( ! defined('FRAMEWORK')) exit('No direct script access allowed');
+<?php if ( ! defined('FRAMEWORK')) exit('No direct script access allowed');
+
 /*********************************************************
- *		Class for Controlling the EMP Framework						
- * 																							
- * 	@Name	class.emp.php											
- * 	@author Eitan																		
- * 																							
- * 	@Version 1.2																	
- * 																							
- * ********************************************************/
+*	Class for Controlling the EMP Framework
+*
+* 	@Name	class.emp.php
+* 	@author Eitan
+*
+* 	@Version 1.2
+*********************************************************/
 
  class Emp{
  		
  	public $_Config;
-
     public $Return;
-
     public $PM;
 
- 	
-	/*************************************************
- 	* 	@name constructor
-	* @desc sets up the initial classes & scripts for the framework
- **************************************************/
-	function __construct(){
+/*************************************************
+* 	@name constructor
+* @desc sets up the initial classes & scripts for the framework
+**************************************************/
+	function __construct(){}
 
-	}
-     /*************************************************
-      * 	@name construct
-      * @desc Custom construct for the class
-      **************************************************/
-     function Construct(){
+/*************************************************
+* 	@name construct
+* @desc Custom construct for the class
+**************************************************/
+     function Construct(){}
 
-     }
-     /*************************************************
-      * 	@name index
-      * @desc default location if no function name was provided
-      **************************************************/
-    function index(){
-        //main index function
-    }
-	/*************************************************
- 	* 	@name load classes
-	* @desc loads the classes for the framework
- **************************************************/	
+/*************************************************
+* 	@name index
+* @desc default location if no function name was provided
+**************************************************/
+    function index(){}
+
+/*************************************************
+* 	@name load classes
+* @desc loads the classes for the framework
+**************************************************/
 	 function load_classes(){
 	 	if(is_dir(CORE_PATH)){
 	 		if($dh=opendir(CORE_PATH)){
@@ -53,7 +47,8 @@
 								if($class[0]!='class') continue;
 								$class=ucfirst($class[1]);
 								if($class != 'Emp'){
-									$this->$class=& new $class();
+                                    $tmpClass= new $class();
+									$this->$class = $tmpClass;
 									$this->$class->_Config=array_merge($this->_Config);
 								}
 							}
@@ -66,11 +61,10 @@
 	 	}
 	}
 	 
-	/*************************************************
- 	* 	@name load config
-	* @desc loads the configuration files for the specific controller
- **************************************************/	
- 	 
+/*************************************************
+* 	@name load config
+* @desc loads the configuration files for the specific controller
+**************************************************/
  	 function load_config($client_path=''){
 		if($client_path='') return;
 		global $conf;
