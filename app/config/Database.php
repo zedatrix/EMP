36553d23d96508e76568a4723555dadda2183ed4
zeddie <zeddicus@myemp.us>
2014-01-07 17:07:57 +0200

    Update Database.php
    Modified database configuration file. Mainly cosmetics.

--- app/config/Database.php
+++ app/config/Database.php
@@ -1,16 +1,16 @@
-<?php  if ( ! defined('FRAMEWORK')) exit('No direct script access allowed');
- /*************************************************
-  * 	@name Database Config File For EMP Framework for php
-  *	@package		EMP
-  * 	@file				Database.php
- * 	@author		We Excel Team
- 
- **************************************************/
- /*************************************************
+<?php if ( ! defined('FRAMEWORK')) exit('No direct script access allowed');
+
+/*************************************************
+* 	@name Database Config File For EMP Framework for php
+*	@package		EMP
+* 	@file			Database.php
+* 	@author		    We Excel Team
+**************************************************/
+
+/*************************************************
  * 	DATABASE
  **************************************************/
 $conf['database']['host']='';
-$conf['database']['identifier']='';
 $conf['database']['database_default']='';
 $conf['database']['username']='';
 $conf['database']['password']='';
